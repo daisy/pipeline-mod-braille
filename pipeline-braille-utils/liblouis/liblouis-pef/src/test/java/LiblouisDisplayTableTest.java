e30589dd701d3973572c9160f4aa7d1e2a68695f
Bert Frees <bertfrees@gmail.com>
2015-05-27 18:03:09 +0200

    Fix liblouis-pef

--- pipeline-braille-utils/liblouis/liblouis-pef/src/test/java/LiblouisDisplayTableTest.java
+++ pipeline-braille-utils/liblouis/liblouis-pef/src/test/java/LiblouisDisplayTableTest.java
@@ -1,15 +1,18 @@
-package org.daisy.pipeline.braille.liblouis.pef;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
-import java.net.URI;
-
 import org.daisy.braille.table.BrailleConverter;
+import org.daisy.braille.table.Table;
 
-import org.daisy.pipeline.braille.liblouis.Liblouis;
-import static org.daisy.pipeline.braille.common.util.URIs.asURI;
+import org.daisy.pipeline.braille.common.Provider;
+import org.daisy.pipeline.braille.common.Provider.DispatchingProvider;
+import org.daisy.pipeline.braille.pef.TableProvider;
 
 import static org.daisy.pipeline.pax.exam.Options.brailleModule;
+import static org.daisy.pipeline.pax.exam.Options.bundlesAndDependencies;
+import static org.daisy.pipeline.pax.exam.Options.domTraversalPackage;
 import static org.daisy.pipeline.pax.exam.Options.felixDeclarativeServices;
 import static org.daisy.pipeline.pax.exam.Options.forThisPlatform;
 import static org.daisy.pipeline.pax.exam.Options.logbackBundles;
@@ -33,9 +36,21 @@ import static org.ops4j.pax.exam.CoreOptions.junitBundles;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
-public class LiblouisDisplayTableBrailleConverterTest {
+public class LiblouisDisplayTableTest {
+	
+	@Test
+	public void testDisplayTableProvider() {
+		Provider<String,Table> tableProvider = getProvider(TableProvider.class);
+		BrailleConverter converter = tableProvider.get("(liblouis-table:'foobar.dis')").iterator().next().newBrailleConverter();
+		assertEquals("⠋⠕⠕⠃⠁⠗", converter.toBraille("foobar"));
+		assertEquals("foobar", converter.toText("⠋⠕⠕⠃⠁⠗"));
+	}
 	
 	@Configuration
 	public Option[] config() {
@@ -43,12 +58,18 @@ public class LiblouisDisplayTableBrailleConverterTest {
 			logbackConfigFile(),
 			logbackBundles(),
 			felixDeclarativeServices(),
+			domTraversalPackage(),
 			mavenBundle().groupId("com.google.guava").artifactId("guava").versionAsInProject(),
 			mavenBundle().groupId("net.java.dev.jna").artifactId("jna").versionAsInProject(),
 			mavenBundle().groupId("org.liblouis").artifactId("liblouis-java").versionAsInProject(),
-			mavenBundle().groupId("org.daisy.libs").artifactId("jing").versionAsInProject(),
+			mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.antlr-runtime").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.libs").artifactId("jstyleparser").versionAsInProject(),
 			mavenBundle().groupId("org.daisy.braille").artifactId("brailleUtils-core").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.libs").artifactId("jing").versionAsInProject(),
+			bundlesAndDependencies("org.daisy.pipeline.calabash-adapter"),
 			brailleModule("common-utils"),
+			brailleModule("css-core"),
+			brailleModule("pef-core"),
 			brailleModule("liblouis-core"),
 			forThisPlatform(brailleModule("liblouis-native")),
 			thisBundle("org.daisy.pipeline.modules.braille", "liblouis-pef"),
@@ -58,12 +79,15 @@ public class LiblouisDisplayTableBrailleConverterTest {
 	}
 	
 	@Inject
-	Liblouis liblouis;
+	private BundleContext context;
 	
-	@Test
-	public void testDisplayTableBrailleConverter() {
-		BrailleConverter converter = new LiblouisDisplayTableBrailleConverter(liblouis.get(new URI[]{asURI("foobar.dis")}));
-		assertEquals("⠋⠕⠕⠃⠁⠗", converter.toBraille("foobar"));
-		assertEquals("foobar", converter.toText("⠋⠕⠕⠃⠁⠗"));
+	private <T> Provider<String,T> getProvider(Class<? extends Provider<String,T>> providerClass) {
+		List<Provider<String,T>> providers = new ArrayList<Provider<String,T>>();
+		try {
+			for (ServiceReference<? extends Provider<String,T>> ref : context.getServiceReferences(providerClass, null))
+				providers.add(context.getService(ref)); }
+		catch (InvalidSyntaxException e) {
+			throw new RuntimeException(e); }
+		return DispatchingProvider.<String,T>newInstance(providers);
 	}
 }
